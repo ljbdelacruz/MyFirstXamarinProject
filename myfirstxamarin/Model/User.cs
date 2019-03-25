41d245459fd43e6cb70e9676541fc10cdafcc142
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-03-25 22:06:19 +0800

    initial

---
+++ myfirstxamarin/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace myfirstxamarin.Model
+{
+    public class User
+    {
+        public String Username { get; set; };
+        public String Password { get; set; }
+        
+
+        public User()
+        {
+        }
+    }
+}
